7e01a69970f2072e22634778a28b883d40038ed7
Julio Tobar <julio.tobar@udla.edu.ec>
2018-02-15 19:39:41 -0500

    Proyecto

---
+++ Assets/scripts/Game_Controller.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Game_Controller : MonoBehaviour {
+    public float parallaxSpeed = 0.02f;
+    public RawImage Background;
+    public RawImage plataform;
+    public GameObject uiIdle;
+    public GameObject uiScore;
+    public enum GameState { Idle, Playing,Ended,  };
+    public GameState gameState = GameState.Idle;
+    public GameObject player;
+    public Text pointsText;
+    public Text recordText;
+    public GameObject enemyGenerator;
+
+    private int points = 0; 
+    // Use this for initialization
+    void Start () {
+        recordText.text = "Mejor Puntaje: " + GetMaxScore().ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        bool userAction = Input.GetKeyDown("up") || Input.GetMouseButtonDown(0);
+        //empieza el juego 
+        if (gameState == GameState.Idle && (userAction))
+        {
+            gameState = GameState.Playing;
+            uiIdle.SetActive(false);
+            uiScore.SetActive(true);
+            player.SendMessage("UpdateState", "sprint");
+            enemyGenerator.SendMessage("StartGeneration");
+
+
+
+        }
+        //si ya se esta jugando 
+        else if (gameState == GameState.Playing)
+        {
+
+            Paralax();
+          
+        }
+        else if (gameState == GameState.Ended)
+        {
+
+            if(userAction)
+            {
+                RestartGame();
+            }
+
+        }
+
+
+
+    }
+    void Paralax()
+    {
+        float finalSpeed = parallaxSpeed * Time.deltaTime;
+        Background.uvRect = new Rect(Background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+        plataform.uvRect = new Rect(plataform.uvRect.x + finalSpeed, 0f, 1f, 1f);
+
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("project");
+    }
+
+    public void IncreasePoints()
+    {
+        points++;
+        pointsText.text = points.ToString();
+
+        if (points >= GetMaxScore())
+        {
+
+            recordText.text = "Mejor Puntaje: " + points.ToString();
+            SaveScore(points);
+        }
+    }
+    public int GetMaxScore()
+    {
+        return PlayerPrefs.GetInt("Max Points", 0);
+
+    }
+    public void SaveScore(int currentPoints)
+    {
+        PlayerPrefs.SetInt("Max Points", currentPoints); 
+    }
+}
---
+++ Assets/scripts/Player_Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    public GameObject game;
+    public GameObject enemyGeneration;
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool gamePlaying = game.GetComponent<Game_Controller>().gameState == Game_Controller.GameState.Playing;
+        if (gamePlaying && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0)))
+        {
+            UpdateState("jump");
+        }
+
+
+    }
+
+    public void UpdateState(string state = null )
+    {
+        if (state != null)
+        {
+            animator.Play(state);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            UpdateState("Die");
+            game.GetComponent<Game_Controller>().gameState = Game_Controller.GameState.Ended;
+            enemyGeneration.SendMessage("cancelGeneration",true);
+
+        }else if (other.gameObject.tag == "point")
+        {
+
+            game.SendMessage("IncreasePoints");
+        }
+    }
+}
